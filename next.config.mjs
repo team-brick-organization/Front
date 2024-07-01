218e81b2d1c54f41d852b8b72bb2bb15b0a4c2d3
lsc58461 <lsc58461@naver.com>
2024-07-01 14:18:27 +0900

    KAN-3-fix: 빌드 에러 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // Cloudflare Workers를 위한 설정
-  experimental: {
-    runtime: 'experimental-edge',
-  },
 }
 
 export default nextConfig
