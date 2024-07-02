32b7d0627b712f43f6e6c655cc42ab5b2ea53330
DongKyuK12 <jkdk981024@gmail.com>
2024-07-02 18:06:33 +0900

    KAN-11-feat : PeopleCounter (인원수프로그레스바)초안, Radix UI Progress install

---
+++ components/PeopleCounter.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import AnimatedProgress from './AnimatedProgress'
+
+interface PeopleCounterProps {
+  current: number
+  min: number
+  max: number
+}
+function PeopleCounter({ current, min, max }: PeopleCounterProps) {
+  const minPersent = (min / max) * 100
+
+  return (
+    <div className="flex h-fit justify-center">
+      <div className="flex w-200pxr flex-col gap-2pxr">
+        <div className="relative">
+          <AnimatedProgress min={min} current={current} max={max} />
+          <div className="h-10pxr w-full border-x-[2px]">
+            <div
+              className="absolute h-10pxr -translate-x-1/2 border-x-[1px]"
+              style={{ left: `${minPersent}%` }}
+            />
+          </div>
+          <p className="font-6pxr absolute -translate-x-1/2 transform">0</p>
+          <p
+            className="font-6pxr absolute -translate-x-1/2"
+            style={{ left: `${minPersent}%` }}
+          >
+            {min}
+          </p>
+          <p className="font-6pxr absolute right-0pxr translate-x-1/2 transform">
+            {max}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PeopleCounter
