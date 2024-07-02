e7b78f2b4945da94b496298c0f60522bc3b87b2f
lsc58461 <lsc58461@naver.com>
2024-07-02 15:36:40 +0900

    KAN-10-fix: page 파일 원래대로 초기화

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,3 @@
-'use client'
-
-import useTab from '@/hooks/useTab'
-import Tab from '@/components/Tab'
-
 export default function Home() {
-  const { activeTab, setActiveTab, tabNames } = useTab({
-    tabNames: ['달램핏', '워케이션'],
-  })
-  return (
-    <div className="p-12pxr">
-      <Tab
-        activeTab={activeTab}
-        setActiveTab={setActiveTab}
-        tabNames={tabNames}
-      />
-    </div>
-  )
+  return <div>Hello world!</div>
 }
