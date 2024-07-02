044d39216b953d6a1dd10b4f8889c9eee5e7b865
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2024-07-02 10:23:58 +0900

    KAN-5-chore: tailwind pxr 단위 적용 (#3)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,36 @@
 import type { Config } from 'tailwindcss'
 
+type AccType = Record<string, string>
+
+const range = (start: number, end: number): number[] => {
+  const array = []
+  for (let i = start; i <= end; i += 1) {
+    array.push(i)
+  }
+  return array
+}
+
+const pxToRem = (px: number, base = 16) => `${px / base}rem`
+
 const config = {
-  darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
   ],
-  prefix: '',
   theme: {
+    spacing: {
+      ...range(0, 2000).reduce((acc: AccType, px: number) => {
+        acc[`${px}pxr`] = pxToRem(px)
+        return acc
+      }, {}),
+    },
+    fontSize: {
+      ...range(0, 2000).reduce((acc: AccType, px: number) => {
+        acc[`${px}pxr`] = pxToRem(px)
+        return acc
+      }, {}),
+    },
     container: {
       center: true,
       padding: '2rem',
