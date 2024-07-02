c7af037a21b6c7df2995509d7e91c4ca78dca29d
lsc58461 <lsc58461@naver.com>
2024-07-02 15:30:32 +0900

    KAN-10-feat: Tab 컴포넌트 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+'use client'
+
+import useTab from '@/hooks/useTab'
+import Tab from '@/components/Tab'
+
 export default function Home() {
-  return <div>Hello world!</div>
+  const { activeTab, setActiveTab, tabNames } = useTab({
+    tabNames: ['달램핏', '워케이션'],
+  })
+  return (
+    <div className="p-12pxr">
+      <Tab
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        tabNames={tabNames}
+      />
+    </div>
+  )
 }
