ea983974f04e05391646c0ece9e81dcf2b72e47d
lsc58461 <lsc58461@naver.com>
2024-07-03 14:48:44 +0900

    KAN-13-fix: 원래 코드로 되돌림

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-'use client'
-
-import FavoriteButton from '@/components/FavoriteButton'
-import useFavorite from '@/hooks/useFavorite'
-
 export default function Home() {
-  const { isFavoriteClicked, handleFavoriteClick } = useFavorite()
-
-  return (
-    <FavoriteButton
-      isFavoriteClicked={isFavoriteClicked}
-      onFavoriteClick={handleFavoriteClick}
-    />
-  )
+  return <div>Hello world!</div>
 }
