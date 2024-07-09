0e0ea329e034ccc63877e1c6f2e296a454ff0fdb
DongKyuK12 <jkdk981024@gmail.com>
2024-07-09 11:32:12 +0900

    BRICK-16-feat: GNB 와이어프레임 기반 제작

---
+++ components/Gnb/Gnb.tsx
@@ -0,0 +1,88 @@
+'use client'
+
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import useSearchStore from '@/store/useSearchStore'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
+import Search from './Search'
+
+/**
+ * GNB컴포넌트
+ *@todo 링크 등록, 디자인, 로고넣기
+ */
+
+function Gnb() {
+  const { onSearch, setOnSearch } = useSearchStore()
+  const [onSearchOpacity, setOnSearchOpacity] = useState<
+    'opacity-0 pointer-events-none' | ''
+  >('')
+
+  const handleOnSearch = () => {
+    setOnSearch(true)
+  }
+
+  useEffect(() => {
+    if (onSearch) {
+      setOnSearchOpacity('opacity-0 pointer-events-none')
+    } else {
+      setOnSearchOpacity('')
+    }
+  }, [onSearch])
+
+  return (
+    <div className="relative w-full bg-gray-200">
+      <main
+        className={`${onSearchOpacity} mx-auto h-72pxr w-full max-w-1180pxr`}
+      >
+        <div className="flex h-full flex-row items-center justify-between">
+          <div className="flex w-full max-w-454pxr flex-row items-center gap-46pxr">
+            <Link href="/">
+              <div className="h-30pxr w-100pxr bg-slate-600" />
+            </Link>
+            <section className="flex flex-row gap-24pxr">
+              <Link href="/my">
+                <p className="font-16pxr">크루</p>
+              </Link>
+              <Link href="/my">
+                <p className="font-16pxr">소셜</p>
+              </Link>
+              <Link href="/my">
+                <p className="font-16pxr">나의 찜</p>
+              </Link>
+            </section>
+          </div>
+          <div className="flex flex-row items-center gap-16pxr">
+            <div>
+              <MagnifyingGlassIcon
+                width="24"
+                height="24"
+                onClick={handleOnSearch}
+              />
+            </div>
+            <Link href="/등록하기">
+              <Button className="h-32pxr w-77pxr rounded-full bg-slate-500">
+                등록하기
+              </Button>
+            </Link>
+            <Link href="/login">
+              <p className="font-16pxr">로그인</p>
+            </Link>
+          </div>
+        </div>
+      </main>
+      <div className="absolute top-0pxr">
+        {onSearch ? (
+          <>
+            <Search />
+            <div className="relative h-screen w-screen bg-black opacity-30" />
+          </>
+        ) : (
+          ''
+        )}{' '}
+      </div>
+    </div>
+  )
+}
+
+export default Gnb
