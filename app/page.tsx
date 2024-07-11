f35f062ad5ab91b6de29f4a0d3587f38e9e1de83
ashtonjo <hee576@nate.com>
2024-07-11 14:45:33 +0900

    BRICK-53-feat: 비밀번호 찾기 페이지 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,3 @@
-'use client'
-
-import { CrewReviewWriteModal, Portal } from '@/components'
-import usePortal from '@/hooks/usePortal'
-
 export default function Home() {
-  const { portalRef, isPortalOpen, setIsPortalOpen, handleOutsideClick } =
-    usePortal()
-  return (
-    <div>
-      <button onClick={() => setIsPortalOpen(true)} type="button">
-        Open Modal
-      </button>
-      <Portal
-        isPortalOpen={isPortalOpen}
-        handleOutsideClick={handleOutsideClick}
-        portalRef={portalRef}
-      >
-        <CrewReviewWriteModal />
-      </Portal>
-    </div>
-  )
+  return <div>Hello world!</div>
 }
