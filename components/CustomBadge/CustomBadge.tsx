f70fbe6a8c6b35654cf771ea8c7f780a899b017c
lsc58461 <lsc58461@naver.com>
2024-07-11 18:37:45 +0900

    BRICK-45-feat: 커스텀 뱃지 컴포넌트 search 타입 추가

--- components/CustomBadge/CustomBadge.tsx
+++ components/CustomBadge/CustomBadge.tsx
@@ -1,17 +1,26 @@
+import { Cross1Icon } from '@radix-ui/react-icons'
 import { Badge } from '@radix-ui/themes'
+import { ReactNode } from 'react'
 
 interface IBadgeProps {
-  type: 'primary' | 'tag'
+  type: 'primary' | 'tag' | 'search'
   size?: 'small' | 'medium' | 'large'
   className?: string
-  children: string
+  onCrossClick?: () => void
+  children: ReactNode
 }
 
 /**
  * type을 primary로 설정하면 size는 small, medium, large로 설정 가능
  */
 
-function CustomBadge({ type, size, className, children }: IBadgeProps) {
+function CustomBadge({
+  type,
+  size,
+  className,
+  onCrossClick,
+  children,
+}: IBadgeProps) {
   const sizeClass = () => {
     switch (size) {
       case 'small':
@@ -28,14 +37,30 @@ function CustomBadge({ type, size, className, children }: IBadgeProps) {
     }
   }
 
-  const typeClass =
-    type === 'tag'
-      ? 'rounded-[0.3125rem] border border-[#DDDEE0] bg-transparent px-10pxr py-4pxr text-[#DDDEE0] font-caption-02'
-      : sizeClass()
+  const typeClass = () => {
+    switch (type) {
+      case 'primary':
+        return sizeClass()
+
+      case 'tag':
+        return 'rounded-[0.3125rem] border border-[#DDDEE0] bg-transparent px-10pxr py-4pxr text-[#DDDEE0] font-caption-02'
+
+      case 'search':
+        return 'px-14pxr py-4pxr !bg-[#F3F4F6] flex gap-8pxr items-center w-fit rounded-full text-[#5E5E60]'
+
+      default:
+        return sizeClass()
+    }
+  }
 
   return (
-    <Badge className={`bg-[#1E1F20] ${typeClass} ${className}`}>
+    <Badge className={`bg-[#1E1F20] ${typeClass()} ${className}`}>
       {children}
+      {type === 'search' && (
+        <button className="outline-none" type="button" onClick={onCrossClick}>
+          <Cross1Icon width={15} height={15} />
+        </button>
+      )}
     </Badge>
   )
 }
