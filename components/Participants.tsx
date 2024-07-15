3653bb3f22fa068cf790de2adac0ec02c3bb5337
lsc58461 <79037820+lsc58461@users.noreply.github.com>
2024-07-15 09:18:30 +0900

    BRICK-45-feat: 소셜 소개 컴포넌트 구현 (#30)
    * BRICK-45-feat: 소셜 소개 컴포넌트 구현
    
    * BRICK-45-chore: 파일이름 수정
    
    * BRICK-45-feat: 커스텀 뱃지 컴포넌트 구현
    
    * BRICK-45-feat: 커스텀 뱃지 컴포넌트 search 타입 추가
    
    * BRICK-45-feat: page 원래대도 돌림
    
    * BRICK-45-fix: 빌드에러 수정

---
+++ components/Participants.tsx
@@ -0,0 +1,43 @@
+import { Avatar } from '@radix-ui/themes'
+import CustomBadge from './CustomBadge/CustomBadge'
+
+interface IParticipantsProps {
+  profileImage: string
+  name: string
+  description: string
+  role: 'host' | 'participant'
+}
+
+function Participants({
+  profileImage,
+  name,
+  description,
+  role,
+}: IParticipantsProps) {
+  return (
+    <div className="flex gap-16pxr">
+      <Avatar
+        className="h-45pxr w-45pxr rounded-full bg-[#D9D9D9]"
+        src={profileImage}
+        fallback={
+          <div className="h-45pxr w-45pxr content-center rounded-full bg-[#D9D9D9] text-center text-14pxr text-white">
+            BRICK
+          </div>
+        }
+      />
+      <div className="flex flex-col gap-4pxr">
+        <div className="flex items-center gap-8pxr">
+          <h3 className="font-body-02">{name}</h3>
+          {role === 'host' && (
+            <CustomBadge type="primary" size="small">
+              호스트
+            </CustomBadge>
+          )}
+        </div>
+        <p className="text-[#DDDEE0] font-caption-02">{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Participants
---
+++ components/ParticipantsList.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useState } from 'react'
+import { Participants } from './index'
+
+interface IParticipantsListProps {
+  participants: {
+    profileImage: string
+    name: string
+    description: string
+    role: 'host' | 'participant'
+  }[]
+}
+
+function ParticipantsList({ participants }: IParticipantsListProps) {
+  const [offset, setOffset] = useState(10)
+
+  const sliceParticipants = participants.slice(0, offset)
+
+  const handleLoadMoreClick = () => {
+    setOffset((prevOffset) => prevOffset + 10)
+  }
+
+  return (
+    <div className="flex flex-col justify-center">
+      <h2 className="text-[#1E1F20] font-title-04">
+        참여자 <span className="text-[#B9BABC]">{participants.length}</span>
+      </h2>
+      <ul className="mt-20pxr flex flex-col gap-16pxr">
+        {sliceParticipants.map((participant) => {
+          const { profileImage, name, description, role } = participant
+
+          return (
+            <li key={profileImage}>
+              <Participants
+                profileImage={profileImage}
+                name={name}
+                description={description}
+                role={role}
+              />
+            </li>
+          )
+        })}
+      </ul>
+      <button
+        className="mt-24pxr text-[#717274] font-body-02"
+        type="button"
+        onClick={handleLoadMoreClick}
+      >
+        더보기
+      </button>
+    </div>
+  )
+}
+
+export default ParticipantsList
