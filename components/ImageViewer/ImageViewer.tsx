297d9281a5fd2fd3705b082ff7102ddc4be615a0
AshtonJo <hee576@nate.com>
2024-07-17 15:42:29 +0900

    BRICK-72-feat: 포탈을 이용한 이미지 뷰어 모달창 반응형  구현 및 사이즈 조정 (#37)
    * test
    
    * BRICK-24-feat: "이미지 뷰어 모달창 구현, api연동x=>목데이터로
    
    * BRICK-72-feat: 이미지 뷰어 동적 반응형 모달창 구현
    
    * BRICK-72-feat: moreImg로 변경
    
    * BRICK-72-feat: moreImg로 재변경
    
    * BRICK-72-feat: mockImages.ts 생성
    
    * BRICK-72-feat: img props 이용해서 받아서 사용하게 변경

--- components/ImageViewer/ImageViewer.tsx
+++ components/ImageViewer/ImageViewer.tsx
@@ -3,69 +3,79 @@
 import { Box } from '@radix-ui/themes'
 import Image from 'next/image'
 import usePortal from '@/hooks/usePortal'
-import { ImageViewerModal, Portal } from '../index'
+import { ImageViewerModal, Portal } from '@/components'
+import moreImgIcon from '@/public/images/svgs/moreImg.svg'
 
-const mockImages = [
-  {
-    id: 1,
-    src: 'https://cdn.pixabay.com/photo/2024/02/15/15/46/cat-8575641_1280.jpg',
-  },
-  {
-    id: 2,
-    src: 'https://cdn.pixabay.com/photo/2023/12/04/16/12/leaf-8429779_1280.jpg',
-  },
-  {
-    id: 3,
-    src: 'https://cdn.pixabay.com/photo/2024/01/02/13/28/gecko-8483282_1280.png',
-  },
-  {
-    id: 4,
-    src: 'https://cdn.pixabay.com/photo/2024/03/28/15/59/life-8661191_1280.jpg',
-  },
-  {
-    id: 5,
-    src: 'https://cdn.pixabay.com/photo/2024/03/28/15/59/life-8661191_1280.jpg',
-  },
-]
+interface IImage {
+  id: number
+  src: string
+}
 
-function ImageViewer() {
+interface ImageViewerProps {
+  images: IImage[]
+}
+
+function ImageViewer({ images }: ImageViewerProps) {
   const { portalRef, isPortalOpen, setIsPortalOpen, handleOutsideClick } =
     usePortal()
 
+  const mobileRemainingImages = images.length - 1
+  const remainingImages = images.length - 5
   return (
     <div className="flex gap-20pxr">
-      <Box className="h-400pxr w-680pxr">
+      <Box className="relative h-400pxr w-680pxr cursor-pointer items-center overflow-hidden rounded-[10px] mb:h-200pxr mb:min-w-380pxr tb:h-301pxr tb:w-319pxr">
         <Image
-          src={mockImages[0].src}
+          src={images[0].src}
           alt="대표 이미지"
-          width={680}
-          height={400}
-          className="h-400pxr w-680pxr object-cover"
+          fill
+          className="h-400pxr w-680pxr object-cover mb:h-200pxr mb:w-full tb:h-301pxr tb:w-319pxr"
           onClick={() => setIsPortalOpen(true)}
         />
+        {mobileRemainingImages > 0 && (
+          <div className="mb:absolute mb:left-1/2 mb:top-1/2 mb:flex mb:-translate-x-1/2 mb:-translate-y-1/2 mb:flex-col mb:items-center mb:justify-center tb:hidden">
+            <Image src={moreImgIcon} alt="icon" width={40} height={40} />
+            <span className="text-center text-[#F9FAFC] font-headline-03">
+              +{mobileRemainingImages}
+            </span>
+          </div>
+        )}
       </Box>
-      <Box className="grid h-400pxr w-480pxr grid-cols-2 grid-rows-2 gap-10pxr">
-        {mockImages.slice(1).map((image) => (
-          <Image
-            key={image.id}
-            src={image.src}
-            alt={image.id.toString()}
-            width={235}
-            height={195}
-            className="h-195pxr w-235pxr object-cover"
-            onClick={() => setIsPortalOpen(true)}
-          />
-        ))}
+      <Box className="grid h-400pxr w-480pxr grid-cols-2 grid-rows-2 gap-10pxr mb:hidden tb:h-301pxr tb:w-319pxr">
+        {images.slice(1, 5).map((image, index) => {
+          return (
+            <div
+              className="relative cursor-pointer overflow-hidden rounded-[10px]"
+              key={image.id}
+            >
+              <Image
+                src={image.src}
+                alt={image.id.toString()}
+                width={235}
+                height={195}
+                className="h-195pxr w-235pxr object-cover"
+                onClick={() => setIsPortalOpen(true)}
+              />
+              {index === 3 && remainingImages > 0 && (
+                <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center">
+                  <Image src={moreImgIcon} alt="icon" width={40} height={40} />
+                  <span className="text-center text-[#F9FAFC] font-headline-03">
+                    +{remainingImages}
+                  </span>
+                </div>
+              )}
+            </div>
+          )
+        })}
       </Box>
 
       <Portal
         handleOutsideClick={handleOutsideClick}
         isPortalOpen={isPortalOpen}
         portalRef={portalRef}
-        className="h-635pxr w-full max-w-1180pxr"
+        className="h-635pxr w-full max-w-1180pxr mb:h-183pxr mb:px-20pxr tb:h-354pxr tb:px-80pxr"
       >
         <ImageViewerModal
-          images={mockImages}
+          images={images}
           initialIndex={0}
           onClose={() => setIsPortalOpen(false)}
         />
