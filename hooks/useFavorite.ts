48bd3604c8068444afb40d5afa964f9f48aa7b4a
DongKyuK12 <jkdk981024@gmail.com>
2024-07-17 13:23:44 +0900

    BRICK-63-fix: useFavorite 로직 수정(로컬스토리지에 저장하는 기능)

--- hooks/useFavorite.ts
+++ hooks/useFavorite.ts
@@ -1,11 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-function useFavorite(initialState = false) {
-  const [isFavoriteClicked, setIsFavoriteClicked] = useState(initialState)
+function useFavorite(id: number) {
+  const [isFavoriteClicked, setIsFavoriteClicked] = useState(false)
 
   const handleFavoriteClick = () => {
-    setIsFavoriteClicked(!isFavoriteClicked)
+    const favoriteSocials = localStorage.getItem('favoriteSocials')
+
+    if (isFavoriteClicked) {
+      if (favoriteSocials) {
+        const filteredIds = JSON.parse(favoriteSocials).filter(
+          (targetId: number) => targetId !== id,
+        )
+        localStorage.setItem('favoriteSocials', JSON.stringify(filteredIds))
+        setIsFavoriteClicked(false)
+      }
+    } else {
+      if (favoriteSocials) {
+        const favoriteIds = JSON.parse(favoriteSocials)
+        favoriteIds.unshift(id)
+        localStorage.setItem('favoriteSocials', JSON.stringify(favoriteIds))
+      }
+      setIsFavoriteClicked(true)
+    }
   }
+
+  useEffect(() => {
+    const favoriteSocials = localStorage.getItem('favoriteSocials')
+
+    if (favoriteSocials) {
+      const localData = JSON.parse(favoriteSocials)
+      const favorite = localData.includes(id)
+      setIsFavoriteClicked(favorite)
+    } else {
+      localStorage.setItem('favoriteSocials', JSON.stringify([]))
+      setIsFavoriteClicked(false)
+    }
+  }, [id])
 
   return { isFavoriteClicked, handleFavoriteClick }
 }
