2728aedebd92b6133dbea302dffbb9482649ba65
ashtonjo <hee576@nate.com>
2024-07-17 14:45:24 +0900

    BRICK-67-feat: password-find 최상위 div px값 추가

--- app/(auth)/password-find/page.tsx
+++ app/(auth)/password-find/page.tsx
@@ -2,7 +2,7 @@ import { PasswordFindForm } from '@/components'
 
 function passwordfind() {
   return (
-    <div className="flex justify-center py-80pxr">
+    <div className="flex w-full justify-center px-20pxr py-80pxr">
       <PasswordFindForm />
     </div>
   )
