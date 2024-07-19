cd9c522a733b08f4036d8e5d239a3407acdbaa0e
ashtonjo <hee576@nate.com>
2024-07-19 14:07:40 +0900

    BRICK-66-feat: 마이페이지 정보 변경 + 프로필 변경 컴포넌트+ 반응형

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import gobackIcon from '@/public/images/svgs/goback.svg'
+
 import {
   Gnb,
   ManagingMyInfo,
@@ -7,6 +9,7 @@ import {
   ProfileImageChangeModal,
 } from '@/components'
 import usePortal from '@/hooks/usePortal'
+import Image from 'next/image'
 
 export default function Home() {
   const { portalRef, isPortalOpen, setIsPortalOpen, handleOutsideClick } =
@@ -14,12 +17,29 @@ export default function Home() {
   return (
     <div>
       <Gnb />
-      <ManagingMyInfo setIsPortalOpen={setIsPortalOpen} />
+      <div>
+        <div className="hidden mb:flex mb:justify-between mb:px-20pxr mb:pb-10pxr mb:pt-12pxr">
+          <button type="button" title="뒤로가기 버튼">
+            <Image
+              src={gobackIcon}
+              alt="뒤로가기 아이콘"
+              width={26}
+              height={26}
+            />
+          </button>
+          <h1 className="text-gray-10 font-title-04">내 정보 관리</h1>
+          <div className="w-26pxr" />
+        </div>
+      </div>
+      <div className="flex h-full w-full py-80pxr pl-20pxr pr-470pxr mb:pl-20pxr mb:pr-22pxr tb:pr-20pxr">
+        <ManagingMyInfo setIsPortalOpen={setIsPortalOpen} />
+      </div>
+
       <Portal
         handleOutsideClick={handleOutsideClick}
         isPortalOpen={isPortalOpen}
         portalRef={portalRef}
-        className="h-full w-full max-w-580pxr mb:px-20pxr"
+        className="h-full w-full max-w-580pxr bg-gray-02 mb:px-20pxr"
       >
         <ProfileImageChangeModal onClose={() => setIsPortalOpen(false)} />
       </Portal>
--- components/ManagingMyInfo.tsx
+++ components/ManagingMyInfo.tsx
@@ -1,24 +1,205 @@
 import Image from 'next/image'
 import pencilIcon from '@/public/images/svgs/pencil.svg'
+import { useForm } from 'react-hook-form'
+import {
+  emailPattern,
+  introduceOneLinePattern,
+  namePattern,
+  nicknamePattern,
+} from '@/constants/RegExr'
+import { Avatar, Button } from '@radix-ui/themes'
+import useProfileImageStore from '@/stores/useProfileImageStore'
+import DatePicker from 'react-datepicker'
+import useDate from '@/hooks/useDate'
+import Input from './Input'
+import SocialDateTimeButton from './SocialDateTimeButton'
 
 interface IManagingMyInfoProps {
   setIsPortalOpen: (value: boolean) => void
 }
 
-function ManagingMyInfo({ setIsPortalOpen }: IManagingMyInfoProps) {
+interface IProfileFormInputs {
+  name: string
+  introduceOneLine: string
+  email: string
+  phoneNumber: number
+  nickname: string
+  birth: number
+}
+
+function ManagingMyInfo({
+  setIsPortalOpen,
+}: IManagingMyInfoProps): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IProfileFormInputs>({ mode: 'onChange' })
+
+  const { selectedDateTime, setSelectedDateTime } = useDate({
+    timeIntervals: 60,
+  })
+  const profileImage = useProfileImageStore((state) => state.profileImage)
+
+  const onSubmit = (data: IProfileFormInputs) => {
+    console.log('정보 변경 데이터', data)
+  }
+
+  const watchAllFields = watch() // 전체 관찰
+
+  // 단 하나라도 필드가 채워졌는지 확인하는 로직
+  const isAnyFieldFilled = Object.values(watchAllFields).some(
+    (value) => value !== undefined && value !== '',
+  )
   return (
-    <form className="max-w-984prx h-1008pxr w-full bg-[#F3F4F6]">
-      <div className="mb-42pxr">
-        <h1 className="text-gray-10">내 정보 관리</h1>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="max-w-984prx relative w-full"
+    >
+      <div className="mb-40pxr mb:hidden">
+        <h1 className="text-gray-10 font-headline-03">내 정보 관리</h1>
       </div>
 
-      <div className="h-80pxr w-80pxr rounded-full bg-[#9A9B9D]">
-        <Image
-          src={pencilIcon}
-          alt="펜슬 아이콘"
+      <div className="relative w-fit">
+        <button
+          type="button"
+          className="h-80pxr w-80pxr overflow-hidden rounded-full"
+          title="수정 버튼"
           onClick={() => setIsPortalOpen(true)}
-          className="cursor-pointer"
-        />
+        >
+          <Avatar
+            src={profileImage || ''}
+            alt="profileImg"
+            fallback={
+              <div className="h-80pxr w-80pxr rounded-full bg-gray-06" />
+            }
+            className="h-80pxr w-80pxr rounded-full bg-gray-06"
+            onClick={() => setIsPortalOpen(true)}
+          />
+        </button>
+
+        <button
+          type="button"
+          className="absolute bottom-0pxr right-0pxr h-19pxr w-19pxr overflow-hidden rounded-full"
+          title="수정 버튼"
+          onClick={() => setIsPortalOpen(true)}
+        >
+          <Image src={pencilIcon} alt="수정 아이콘" width={19} height={19} />
+        </button>
+      </div>
+
+      <div className="mt-32pxr">
+        <div>
+          <label htmlFor="name" className="mb-8pxr text-gray-10 font-title-02">
+            이름
+          </label>
+          <Input
+            variant="border"
+            id="name"
+            {...register('name', {
+              required: '이름은 필수 입력입니다.',
+              pattern: namePattern,
+            })}
+            type="text"
+            placeholder="김OO"
+            className={`mt-8pxr ${errors.name ? 'ring-1 ring-error' : ''}`}
+          />
+        </div>
+
+        <div className="mt-40pxr">
+          <label
+            htmlFor="introduceOneLine"
+            className="mb-8pxr text-gray-10 font-title-02"
+          >
+            한 줄 소개
+          </label>
+          <Input
+            variant="border"
+            id="introduceOneLine"
+            {...register('introduceOneLine', {
+              required: '한 줄 소개를 입력해 주세요.',
+              pattern: introduceOneLinePattern,
+            })}
+            type="text"
+            placeholder="띄어쓰기 포함 80자 이내로 입력해 주세요."
+            className={`mt-8pxr ${errors.introduceOneLine ? 'ring-1 ring-error' : ''}`}
+          />
+        </div>
+        <div className="mt-40pxr">
+          <label htmlFor="email" className="mb-8pxr text-gray-10 font-title-02">
+            이메일
+          </label>
+          <Input
+            variant="border"
+            id="email"
+            {...register('email', {
+              required: '이메일은 필수 입력입니다.',
+              pattern: emailPattern,
+            })}
+            type="email"
+            placeholder="User@gmail.com"
+            className={`mt-4pxr ${errors.email ? 'ring-1 ring-error' : ''}`}
+            color="gray"
+          />
+        </div>
+
+        <div className="mt-40pxr">
+          <label
+            htmlFor="nickname"
+            className="mb-8pxr text-gray-10 font-title-02"
+          >
+            닉네임
+          </label>
+          <Input
+            variant="border"
+            id="nickname"
+            {...register('nickname', {
+              required: '닉네임은 필수 입력입니다.',
+              pattern: nicknamePattern,
+            })}
+            type="text"
+            placeholder="User123"
+            className={`mt-8pxr ${errors.nickname ? 'ring-1 ring-error' : ''}`}
+          />
+        </div>
+        <div className="mt-40pxr flex flex-col">
+          <label htmlFor="birth" className="mb-8pxr text-gray-10 font-title-02">
+            생년월일
+          </label>
+          <DatePicker
+            className="rounded-[0.3125rem] bg-gray-01 px-20pxr py-14pxr text-start text-gray-10 font-body-02"
+            shouldCloseOnSelect
+            maxDate={new Date()}
+            selected={selectedDateTime}
+            showPopperArrow={false}
+            enableTabLoop={false}
+            customInput={
+              <SocialDateTimeButton
+                type="date"
+                selectedDateTime={selectedDateTime}
+              />
+            }
+            onChange={(date) => setSelectedDateTime(date)}
+            popperPlacement="bottom-start"
+            dateFormatCalendar="yyyy년 M월"
+            locale="ko"
+            timeCaption="시간"
+            showYearDropdown
+            scrollableYearDropdown
+            yearDropdownItemNumber={100}
+          />
+        </div>
+      </div>
+
+      <div className="mb-165pxr mt-192pxr flex justify-center">
+        <Button
+          type="submit"
+          className={`h-46pxr w-full max-w-216pxr cursor-pointer rounded-[0.625rem] ${isAnyFieldFilled ? 'bg-gray-10 text-gray-01' : 'bg-gray-04 text-gray-01'}`}
+          disabled={!isAnyFieldFilled}
+        >
+          저장하기
+        </Button>
       </div>
     </form>
   )
