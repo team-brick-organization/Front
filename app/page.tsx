e41bf2d263eedcd57083f4978aeb8f9ff16b76fb
lsc58461 <lsc58461@naver.com>
2024-07-22 11:07:27 +0900

    BRICK-75-feat: page 원래대로 돌림

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import { Gnb } from '../components'
-
 export default function Home() {
-  return (
-    <>
-      <Gnb />
-      <div>hello world!</div>
-    </>
-  )
+  return <div>Hello world!</div>
 }
