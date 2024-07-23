335147f229456ac92118db74059c95aac29c07c4
DongKyuK12 <jkdk981024@gmail.com>
2024-07-23 17:35:36 +0900

    BRICK-82-test: 더미커밋

--- app/search/[id]/page.tsx
+++ app/search/[id]/page.tsx
@@ -10,7 +10,7 @@ import getSearchResult from '@/apis/getSearchResult'
 import useSearchStore from '@/stores/useSearchStore'
 
 /** search 페이지
- * @todo api 나오면 총 개수 넣어주기, 인기순 최신순 정렬 넣어주기, 페이지네이션 적용
+ * @todo api 나오면 총 개수 넣어주기, 인기순 최신순 정렬 넣어주기,  페이지네이션 적용
  */
 
 function SearchPage() {
