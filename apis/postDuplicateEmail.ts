34a70540ac674e1e5909b6ee1b285c153623a84f
AshtonJo <hee576@nate.com>
2024-07-23 10:06:11 +0900

    BRICK-77-feat: 회원가입 페이지 api 수정+ 디자인 수정 (#53)
    * BRICK-58-feat: 공용 Button Component 구현
    
    * BRICK-81-feat: moreImg Icon 및 remainingImages 변수삭제
    
    * BRICK-77-feat: 회원가입 페이지 api 수정+ 디자인 수정
    
    * BRICK-77-fix: 회원가입 닉네임, 이메일 중복검사 TYPE A,B,C 네이밍 변경

--- apis/postDuplicateEmail.ts
+++ apis/postDuplicateEmail.ts
@@ -1,14 +1,11 @@
+import type { TypeEmail } from 'types/types'
 import BASE_URL from './apiConfig'
-
-interface IPostDuplicateEmailProps {
-  body: {
-    email: string
-  }
-}
 
 async function postDuplicateEmail({
   body,
-}: IPostDuplicateEmailProps): Promise<Response> {
+}: {
+  body: TypeEmail
+}): Promise<Response> {
   const response = await fetch(`${BASE_URL}/auth/users/duplicate-email`, {
     method: 'POST',
     headers: {
---
+++ types/types.ts
@@ -0,0 +1,2 @@
+export type TypeEmail = { email: string }
+export type TypeNickname = { nickname: string }
