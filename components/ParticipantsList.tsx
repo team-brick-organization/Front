158d39180ef7b2e4ca6ab1f46842864d97367f1d
DongKyuK12 <jkdk981024@gmail.com>
2024-08-02 10:33:42 +0900

    BRICK-103-fix: 빌드에러 수정 ( 인터페이스 임포트문 삭제 )

--- components/ParticipantsList.tsx
+++ components/ParticipantsList.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import { IParticipants } from 'types/getSocialDetails'
 import { Participants } from './index'
 
 interface IParticipantsListProps {
